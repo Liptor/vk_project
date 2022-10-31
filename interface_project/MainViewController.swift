0ab465a2e678427909531529ce515ac5aa7ca6e4
Ivan Ingovatov <i.ingovatov@hookprod.com>
2022-11-01 01:35:37 +0300

    [FEAT]: update a new app design

--- interface_project/MainViewController.swift
+++ interface_project/MainViewController.swift
@@ -8,15 +8,21 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginField: UITextField!
+    @IBOutlet weak var labelText: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        self.labelText.text = self.loginField.text!
+    }
+    
 
-    /*
+    /*name
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
