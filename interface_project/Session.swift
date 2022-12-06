791283b1cbd20224785e6b4b0862df67b0865e10
Ivan Ingovatov <i.ingovatov@hookprod.com>
2022-12-06 23:29:20 +0300

    [FEAT]: singleton session

---
+++ interface_project/Session.swift
@@ -0,0 +1,20 @@
+//
+//  Session.swift
+//  interface_project
+//
+//  Created by acer on 06.12.2022.
+//
+
+import Foundation
+
+// Singleton
+
+class Session {
+    
+    static let session = Session()
+    private init() {}
+    
+    let token = ""
+    let userId = 0
+    
+}
